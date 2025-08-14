553dca2c52516ce9ab8213bae7a1fec5a3efc6b3
vaughan <60723221+zeroxoneafour@users.noreply.github.com>
2025-08-14 04:45:07 +0000

    date picker broken right now

--- src/lib/components/ui/DatePicker.svelte
+++ src/lib/components/ui/DatePicker.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-	import type { DueDate } from "$lib/colleges.svelte";
+	import { dueDateToDate, type DueDate } from "$lib/colleges.svelte";
 
     let { date = $bindable() }: { date: DueDate } = $props();
+
+    function dueDateToString(dueDate: DueDate): string {
+        const date = dueDateToDate(dueDate);
+        const year = String(date.getFullYear()).padStart(4, "0");
+        const month = String(date.getMonth()).padStart(2, "0");
+        const day = String(date.getDay()).padStart(2, "0");
+        return [year, month, day].join("-");
+    }
+    function dueDateFromString(str: string): DueDate {
+        let arr = str.split("-").map(x => Number(x));
+        return {
+            Month: arr[1] - 1,
+            Day: arr[2]
+        };
+    }
 </script>
 
-<input type="date" class="input" bind:value={date}/>
+<input type="date" class="input" bind:value={() => dueDateToString(date), (v) => (date = dueDateFromString(v))} />
